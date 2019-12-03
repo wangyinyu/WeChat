ffd18b0071e6552979c44d9def00001878ea63af
wangyinyu <821498921@qq.com>
2019-12-03 22:43:27 +0800

    todolist

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <input type="text" v-model="msg"> <button @click="add()">添加数据</button>
+  <h2>{{msg}}</h2>
+  <br>
+  <ul>
+    <li v-for="(item,key) in list">
+      {{item}}<button @click="remove(key)">删除数据</button>
+    </li>
+  </ul>
   </div>
 </template>
 
@@ -24,7 +16,16 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'vue学习开始',
+      list:[]
+    }
+  },
+  methods:{
+    add(){
+        this.list.push(this.msg)
+    },
+    remove(key){
+        this.list.splice(key,1);
     }
   }
 }
@@ -38,23 +39,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
