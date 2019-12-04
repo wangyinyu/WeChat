f6c61566c9d6db93b2f1e810ab589f2d1d735549
sichen <ava95727@sina.cn>
2019-12-04 21:46:25 +0800

    ul样式修改

--- src/App.vue
+++ src/App.vue
@@ -52,8 +52,17 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 5%;
+}
+ul{
+  text-align: left;
+  /* margin-left: 40%; */
+}
+ul li{
+  margin: 5px 0;
+  list-style: none;
 }
 </style>
